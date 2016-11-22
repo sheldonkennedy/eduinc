2efc64005a90539ec397cad5073add75a641111a
ldupisani <louisdupisani@yahoo.co.uk>
2016-11-22 14:40:16 +0200

    wiring up html to js

--- assets/js/_fees.js
+++ assets/js/_fees.js
@@ -1,23 +1,30 @@
 function setRadios() {
 
-  var inputs = document.getElementById('m4jForm').getElementsByClassName('m4jRadio');
+    var inputs = document.getElementById('m4jForm').getElementsByClassName('m4jRadio');
 
-  function sumRadios() {
-    var total = 0,
-      oForm = this.form;
+    function sumRadios() {
+        var total = 0,
+            oForm = this.form;
 
-    for (var x = 0; x < inputs.length; x++) {
-      if (inputs[x].checked) total += parseInt(inputs[x].value);
+        for (var x = 0; x < inputs.length; x++) {
+            if (inputs[x].checked) total += parseInt(inputs[x].value);
+        }
+
+        // Defines the field that totals the selections
+        oForm.elements['m4j65'].value = total.toFixed(0);
+
     }
 
-    // Defines the field that totals the selections
-    oForm.elements['m4j65'].value = total.toFixed(0);
-
-  }
-
-  for (var y = 0; y < inputs.length; y++) {
-    inputs[y].onclick = sumRadios;
-  }
+    for (var y = 0; y < inputs.length; y++) {
+        inputs[y].onclick = sumRadios;
+    }
 
 }
 onload = setRadios;
+
+
+function calculateFees() {
+
+    alert("CALCULATE FEES HERE");
+
+}
