32e9f45cb67e004f0be001dd87cb9d32d52682e0
ldupisani <louisdupisani@yahoo.co.uk>
2016-11-22 19:21:57 +0200

    calculations sans formatting and rounding

--- assets/js/_fees.js
+++ assets/js/_fees.js
@@ -22,6 +22,18 @@ function setRadios() {
 }
 onload = setRadios;
 
+feeValues = {
+     "4" : { monthly: 6264.00, annual:  6781.26, registration: 5312.40, deposit: 6264.00},
+     "5" : { monthly: 6264.00, annual:  6866.68, registration: 5312.40, deposit: 6264.00},
+     "6" : { monthly: 6264.00, annual:  7021.55, registration: 5312.40, deposit: 6264.00},
+     "7" : { monthly: 7606.00, annual:  7693.84, registration: 5312.40, deposit: 7606.00},
+     "8" : { monthly: 7606.00, annual:  8168.81, registration: 5312.40, deposit: 7606.00},
+     "9" : { monthly: 7606.00, annual:  8607.12, registration: 5312.40, deposit: 7606.00},
+    "10" : { monthly: 8920.00, annual:  8779.71, registration: 5312.40, deposit: 8920.00},
+    "11" : { monthly: 8920.00, annual: 12837.01, registration: 5312.40, deposit: 8920.00},
+    "12" : { monthly: 8920.00, annual:  8983.08, registration: 5312.40, deposit: 8920.00}
+}
+
 
 function calculateFees() {
 
@@ -30,14 +42,56 @@ function calculateFees() {
     var optionalFET = $("#optionalFET").val();
     var paymentFrequency = $("#paymentFrequency").val();
 
-    alert("CALCULATE FEES HERE SELECTS: " + enrolment + " | " + enteringGrade + " | " + optionalFET + " | " + paymentFrequency);
+    var total = 0.0
 
-    $("#registration").val("based on some calculation");
-    $("#deposit").val("based on some calculation");
-    $("#tuition").val("based on some calculation");
-    $("#curriculum").val("based on some calculation");
-    $("#fetsubjects").val("based on some calculation");
-    $("#total").val("based on some calculation");
+    if (enteringGrade != "") {
+
+        if (enrolment == "newstudent") {
+
+            total += feeValues[enteringGrade].registration;
+            $("#registration").val(feeValues[enteringGrade].registration);
+
+        } else {
+
+            $("#registration").val(0);
+
+        }
+
+        total += feeValues[enteringGrade].deposit;
+        $("#deposit").val(feeValues[enteringGrade].deposit);
+
+        var tuition = feeValues[enteringGrade].monthly * 12;
+        if (paymentFrequency == "perterm") { tuition * 95.0/100.0 }
+        if (paymentFrequency == "peryear") { tuition * 92.0/100.0 }
+
+        total += tuition
+        $("#tuition").val(tuition);
+
+        total += feeValues[enteringGrade].annual;
+        $("#curriculum").val(feeValues[enteringGrade].annual);
+
+
+
+    } else {
+
+        $("#deposit").val("0");
+        $("#tuition").val("0");
+        $("#curriculum").val("0");
+        
+    }
+
+    if (optionalFET == "egd") {
+
+        total += 1687.50;
+        $("#fetsubjects").val("1687.50");
+
+    } else {
+
+        $("#fetsubjects").val("0");
+
+    }
+
+    $("#total").val(total);
 
 
 
