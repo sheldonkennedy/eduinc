21e43f3d15721d2dcb6eca8f3a163b441f584f85
sheldonkennedy <sheldonkennedy@gmail.com>
2016-11-22 14:22:12 +0200

    Education Incorporated 2016 Website

---
+++ assets/js/_fees.js
@@ -0,0 +1,23 @@
+function setRadios() {
+
+  var inputs = document.getElementById('m4jForm').getElementsByClassName('m4jRadio');
+
+  function sumRadios() {
+    var total = 0,
+      oForm = this.form;
+
+    for (var x = 0; x < inputs.length; x++) {
+      if (inputs[x].checked) total += parseInt(inputs[x].value);
+    }
+
+    // Defines the field that totals the selections
+    oForm.elements['m4j65'].value = total.toFixed(0);
+
+  }
+
+  for (var y = 0; y < inputs.length; y++) {
+    inputs[y].onclick = sumRadios;
+  }
+
+}
+onload = setRadios;
