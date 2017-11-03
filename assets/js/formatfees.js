227f7c5b6c87478a5231b0c8ffa686f94fac1e83
ldupisani <louisdupisani@yahoo.co.uk>
2017-11-03 10:58:44 +0200

    move fees formatting to different file

--- assets/js/formatfees.js
+++ assets/js/formatfees.js
@@ -1,3 +1,4 @@
+/*
 Number.prototype.formatMoney = function (c, d, t) {
 	var n = this,
 		c = isNaN(c = Math.abs(c)) ? 2 : c,
@@ -8,3 +9,4 @@ Number.prototype.formatMoney = function (c, d, t) {
 		j = (j = i.length) > 3 ? j % 3 : 0;
 	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
+*/
